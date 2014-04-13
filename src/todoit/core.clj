3af991a9c39cfcb2bd22a0b73497617499974d5b
Ryan Neufeld <ryan@rkn.io>
2014-04-12 22:47:13 -0400

    Server header and /hello exercise solutions

--- src/todoit/core.clj
+++ src/todoit/core.clj
@@ -2,19 +2,22 @@
   (:require [io.pedestal.http.route.definition :refer [defroutes]]
             [io.pedestal.http.route :as route :refer [router]]
             [io.pedestal.http :as http]
-            [io.pedestal.interceptor :refer [defon-request]]
+            [io.pedestal.interceptor :refer [defon-request defon-response]]
             [ns-tracker.core :refer [ns-tracker]]
             [ring.handler.dump :refer [handle-dump]]))
 
 (defn hello-world [req]
   (let [name (get-in req [:query-params :name])]
     {:status 200
-     :body (str "Hello, " name "!")
+     :body (str "Hello, " (or name "World") "!")
      :headers {}}))
 
 (defon-request capitalize-name [req]
   (update-in req [:query-params :name] (fn [name] (when name
                                                     (clojure.string/capitalize name)))))
+
+(defon-response affix-custom-server [resp]
+  (update-in resp [:headers "Server"] (constantly "Computron 9000")))
 
 (defn goodbye-cruel-world [req]
   {:status 200
@@ -22,7 +25,7 @@
    :headers {}})
 
 (defroutes routes
-  [[["/"
+  [[["/" ^:interceptors [affix-custom-server]
      ["/hello" ^:interceptors [capitalize-name] {:get hello-world}]
      ["/goodbye" {:get goodbye-cruel-world}]
      ["/request" {:any handle-dump}]]]])
