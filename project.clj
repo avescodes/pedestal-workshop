fd0c0df04321dc65b8bd903d5436667efd9d9ab1
Ryan Neufeld <ryan@rkn.io>
2014-04-12 22:20:23 -0400

    From zero to hello-world. Create a simple Pedestal service.

--- project.clj
+++ project.clj
@@ -2,4 +2,8 @@
   :url "https://github.com/rkneufeld/pedestal-workshop"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [io.pedestal/pedestal.service "0.3.1"]
+                 [io.pedestal/pedestal.service-tools "0.3.1"]
+                 [io.pedestal/pedestal.jetty "0.3.1"]]
+  :main todoit.core)
--- src/todoit/core.clj
+++ src/todoit/core.clj
@@ -1,6 +1,22 @@
-(ns todoit.core)
+(ns todoit.core
+  (:require [io.pedestal.http.route.definition :refer [defroutes]]
+            [io.pedestal.http.route :refer [router]]
+            [io.pedestal.http :as http]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn hello-world [req]
+  {:status 200
+   :body "Hello, world!"
+   :headers {}})
+
+(defroutes routes
+  [[["/"
+     ["/hello" {:get hello-world}]]]])
+
+(def service
+  {::http/interceptors [(router routes)]
+   ::http/port 8080})
+
+(defn -main [& args]
+  (-> service
+      http/create-server
+      http/start))
